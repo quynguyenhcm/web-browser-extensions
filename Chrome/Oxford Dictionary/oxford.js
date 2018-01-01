f4cdc1897df6c54d19725bbb60133a9b3c67fd1f
quynguyenhcm <nguyenledinhquy@gmail.com>
2018-01-01 09:56:23 +0700

    Fix typo in description and clean up tab characters

--- Chrome/Oxford Dictionary/oxford.js
+++ Chrome/Oxford Dictionary/oxford.js
@@ -6,26 +6,26 @@ chrome.contextMenus.create({
 
 function lookUpOxford(info,tab) {
     if(info){
-    	var token=info.selectionText;
-    	var oxfordUrl = 'https://www.oxfordlearnersdictionaries.com/search/english/?q='+token;
-    	lookUpSelection(token, oxfordUrl);
-   	}     
+        var token=info.selectionText;
+        var oxfordUrl = 'https://www.oxfordlearnersdictionaries.com/search/english/?q='+token;
+        lookUpSelection(token, oxfordUrl);
+    }
 }
 
 function lookUpSelection(text, url){
-	if(text){
-		var w = 550;
-    	var h = 450;
-    	var left = (screen.width/2)-(w/2);
-    	var top = (screen.height/2)-(h/2);
- 		chrome.windows.create({'url': url, 
- 							'type': 'popup',
- 							'width': w,
- 							'height': h,
- 							'left': left, 
- 							'top': top }, function(window) {}
-   							);
-   	}  
+    if(text){
+        var w = 550;
+        var h = 450;
+        var left = (screen.width/2)-(w/2);
+        var top = (screen.height/2)-(h/2);
+        chrome.windows.create({'url': url,
+                            'type': 'popup',
+                            'width': w,
+                            'height': h,
+                            'left': left,
+                            'top': top }, function(window) {}
+                            );
+    }
 }
 // 
 // chrome.tabs.executeScript( {
